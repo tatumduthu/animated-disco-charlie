81a7b3f3cb38bae987e918d405e1cdf0d21fa576
tatumduthu <tatumrduthu@gmail.com>
2025-02-24 23:43:21 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,15 @@
 // stuff
 // w3s: https://www.w3schools.com/JQuery/event_ready.asp
-$(document).ready(function () {
-    console.log("Welcome to Dev Charlie's Past Projects Dashboard! 🦭");
+// $(document).ready(function () {
+//     console.log("Welcome to Dev Charlie's Past Projects Dashboard! 🦭");
 
-    // Easter egg: Secret function in console
-    window.easterEgg = function() {
-        console.log("You found the Easter egg!", "color: purple; font-size: 16px; font-weight: bold;");
-        console.log("You just walked the prank🦭🦭🦭🦭🦭👌");
-    };
+//     // Easter egg: Secret function in console
+//     window.easterEgg = function() {
+//         console.log("You found the Easter egg!", "color: purple; font-size: 16px; font-weight: bold;");
+//         console.log("You just walked the prank🦭🦭🦭🦭🦭👌");
+//     };
 
-    console.log("Type %c easterEgg() %c in the console to discover a surprise! 🦭", "color: green; font-weight: bold;", "");
+//     console.log("Type %c easterEgg() %c in the console to discover a surprise! 🦭", "color: green; font-weight: bold;", "");
 
 // w3s: https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
